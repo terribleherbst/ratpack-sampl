5485c109fe74486d5146a9628a110e84edbf5576
Jörg Herbst <joerg.herbst@10m.de>
2015-03-22 20:57:18 +0100

    Create a simple ratpack REST demo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+This is a very simple application showing how to provide a REST service using ratpack.io.
+You can start it using just
 ```
 ./gradlew run
 ```
--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,10 +1,14 @@
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import groovy.transform.ToString
 import ratpack.jackson.JacksonModule
 
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.fromJson
 import static ratpack.jackson.Jackson.json
 
-class Course {
+@ToString(includeNames=true)
+class Talk {
+    Integer id;
     String title;
     String speaker;
     String company;
@@ -17,58 +21,97 @@ ratpack {
          add(JacksonModule) {JacksonModule.Config config -> config.modules(new Jdk8Module()).prettyPrint(false)}
      }
 
-    talks = [
-            new Course(title: "Lambdas and Streams, Functional Programming in JDK8",
+    courses = [
+            new Talk(
+                    id : 1,
+                    title: "Lambdas and Streams, Functional Programming in JDK8",
                     speaker: "Simon Ritter",
                     company: "Oracle",
                     language: "en",
                     track: "Core Java"),
-            new Course(title: "Understanding Java byte code",
+            new Talk(
+                    id : 2,
+                    title: "Understanding Java byte code",
                     speaker: "Rafael Winterhalter",
                     company: "Kantega AS",
                     language: "en",
                     track: "Core Java"),
-            new Course(title: "Legacy",
+            new Talk(
+                    id: 3,
+                    title: "Legacy",
                     speaker: "Peter Lawrey",
                     company: "Higher Frequency Trading Ltd.",
                     language: "en",
                     track: "Core Java"),
-            new Course(title: "Lessons",
+            new Talk(
+                    id: 4,
+                    title: "Lessons",
                     speaker: "Aron Gupta",
                     company: "Red Hat Inc",
                     language: "en",
                     track: "Enterprise Java"),
-            new Course(title: "JBatch mit Apache BatchEE",
+            new Talk(
+                    id: 5,
+                    title: "JBatch mit Apache BatchEE",
                     speaker: "Mark Strutberg",
                     company: "TU Wien",
                     language: "de",
                     track: "Enterprise Java"),
-            new Course(title: "Where are we with MVC in JavaEE 8",
+            new Talk(
+                    id: 6,
+                    title: "Where are we with MVC in JavaEE 8",
                     speaker: "Manfred Riem",
                     company: "Oracle",
                     language: "en",
                     track: "Enterprise Java"),
-            new Course(title: "2000 Zeilen Java Code oder 50 Zeilen SQL",
+            new Talk(
+                    id: 7,
+                    title: "2000 Zeilen Java Code oder 50 Zeilen SQL",
                     speaker: "Lukas Eder",
                     company: "Data Geekery GmbH",
                     language: "de",
                     track: "Enterprise Java"),
-            new Course(title: "BYOC - Bring Your Own Container",
+            new Talk(
+                    id : 8,
+                    title: "BYOC - Bring Your Own Container",
                     speaker: "Christian Kaltepoth",
                     company: "TU Wien",
                     language: "de",
                     track: "Enterprise Java")
     ]
+    def talks = courses.collectEntries {[it.id,it]}
+
     handlers {
         get("health") {
             ctx -> response.send("yeah")
         }
         handler("talk") {
-            render json(talks)
+            byMethod {
+                get {
+                    render json(talks.values())
+                }
+                post {
+                    def talk = parse (fromJson(Talk))
+                    talks.add(talk)
+                    render json(talk)
+                }
+
+            }
         }
-        handler("talk/:title") {
-            def query = pathTokens["title"]
-            render json (talks.find { talk -> talk.title.contains(query)})
+        handler("talk/:id") {
+            def query = pathTokens["id"]
+            byMethod {
+                get {
+                    render json (talks.find { talk -> talk.id = query})
+                }
+                put {
+                    def talk = parse (fromJson(Talk))
+                    talks.put(query,talk)
+                }
+                delete {
+                    talks.remove(query)
+                }
+            }
         }
     }
 }
